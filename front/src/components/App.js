75675565e1c2349ab1e3157af7a65b3aca84ce70
Alexander Troshchenko <main.snivik@gmail.com>
2019-06-21 10:22:20 +0300

    Added SASS and default description

---
+++ front/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default () => <div className={'intro-box'}>
+
+    <div className={'header'}>Hi there!</div>
+
+    <div className={'description'}>
+        Introducing my own react feature test lab!
+    </div>
+</div>;
